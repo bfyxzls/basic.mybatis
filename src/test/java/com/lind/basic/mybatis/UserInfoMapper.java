b7e464efece10dcbc30bc96094889c94e02d4713
bfyxzls <bfyxzls@sina.com>
2019-09-06 19:56:56 +0800

    add find query

--- src/test/java/com/lind/basic/mybatis/UserInfoMapper.java
+++ src/test/java/com/lind/basic/mybatis/UserInfoMapper.java
@@ -1,6 +1,11 @@
 package com.lind.basic.mybatis;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface UserInfoMapper extends BaseMapper<UserInfo> {
+
+  @Select("select * from user_info where username=@username")
+  UserInfo findByUsername(@Param("username") String username);
 }
